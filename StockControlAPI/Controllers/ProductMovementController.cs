013228c2d10f750bd5f062a90a0decdb30830dbf
Thiago Perine <thiagoauper@gmail.com>
2025-04-15 16:50:07 -0300

    Creating Product Movement Controller and DTO.

---
+++ StockControlAPI/Controllers/ProductMovementController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockControlAPI.DTO;
+using StockControlAPI.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StockControlAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductMovementController : ControllerBase
+    {
+        // POST api/<ProductMovementController>
+        [HttpPost]
+        public void Post([FromBody] ProductMovementDTO movement)
+        {
+            if (!movement.IsValid())
+            {
+                throw new ArgumentException("Invalid product movement data.");
+            }
+
+            ProductMovement productMovement = movement.ToModel();
+
+            //TODO: Persist product movement in the data base!
+        }
+    }
+}
---
+++ StockControlAPI/DTO/ProductMovementDTO.cs
@@ -0,0 +1,22 @@
+﻿using StockControlAPI.Entities;
+using StockControlAPI.Enums;
+
+namespace StockControlAPI.DTO
+{
+    public class ProductMovementDTO
+    {
+        public Guid ProductCode { get; set; }
+        public ProductMovementType MovementType { get; set; }
+        public int Quantity { get; set; }
+
+        internal bool IsValid()
+        {
+            return ProductCode != Guid.Empty && Quantity > 0;
+        }
+
+        internal ProductMovement ToModel()
+        {
+            return new ProductMovement(new Product { Code = ProductCode }, Quantity, MovementType);
+        }
+    }
+}
