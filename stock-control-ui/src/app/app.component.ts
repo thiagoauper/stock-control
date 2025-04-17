d1ed040f201d3e186e0a2d6514c7cbd290239fd1
Thiago Perine <thiagoauper@gmail.com>
2025-04-17 18:34:09 -0300

    Using MatTab to alternate between components.

--- stock-control-ui/src/app/app.component.ts
+++ stock-control-ui/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { AddProductMovementComponent } from '../modules/features/product-movement/components/add-product-movement/add-product-movement.component';
 import { StockReportComponent } from '../modules/features/reports/components/stock-report/stock-report.component';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddProductMovementComponent, StockReportComponent, CommonModule],
+  imports: [RouterOutlet, AddProductMovementComponent, StockReportComponent, CommonModule, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
