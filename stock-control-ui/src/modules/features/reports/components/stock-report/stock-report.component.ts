4d106a6abe0c4028895295313f007450119ba4d1
Thiago Perine <thiagoauper@gmail.com>
2025-04-17 14:12:04 -0300

    Continuing scaffolding Stock Report component.

--- stock-control-ui/src/modules/features/reports/components/stock-report/stock-report.component.ts
+++ stock-control-ui/src/modules/features/reports/components/stock-report/stock-report.component.ts
@@ -20,14 +20,14 @@ export class StockReportComponent {
   fetchStockData() {
     // Simulate fetching stock data from a service or API
     this.stockData = [
-      { productCode: 'Product A', movementDate: new Date(2025, 3, 12), totalInbound: 35, totalOutbound: 15, balance: 20 },
-      { productCode: 'Product B', movementDate: new Date(2025, 3, 12), totalInbound: 50, totalOutbound: 20, balance: 30 },
-      { productCode: 'Product C', movementDate: new Date(2025, 3, 12), totalInbound: 20, totalOutbound: 10, balance: 10 },
-      { productCode: 'Product D', movementDate: new Date(2025, 3, 12), totalInbound: 10, totalOutbound: 5, balance: 5 },
-      { productCode: 'Product E', movementDate: new Date(2025, 3, 12), totalInbound: 15, totalOutbound: 5, balance: 10 },
-      { productCode: 'Product F', movementDate: new Date(2025, 3, 12), totalInbound: 25, totalOutbound: 10, balance: 15 },
-      { productCode: 'Product G', movementDate: new Date(2025, 3, 12), totalInbound: 30, totalOutbound: 20, balance: 10 },
-      { productCode: 'Product H', movementDate: new Date(2025, 3, 12), totalInbound: 40, totalOutbound: 30, balance: 10 },
+      { productName: 'Product A', productCode: '3fa85f64-5717-4562-b3fc-2c963f66afa6', movementDate: new Date(2025, 3, 12), totalInbound: 35, totalOutbound: 15, balance: 20 },
+      { productName: 'Product B', productCode: '3fa85f64-5717-4562-b3fc-2c963f66afa6', movementDate: new Date(2025, 3, 12), totalInbound: 50, totalOutbound: 20, balance: 30 },
+      { productName: 'Product C', productCode: '3fa85f64-5717-4562-b3fc-2c963f66afa6', movementDate: new Date(2025, 3, 12), totalInbound: 20, totalOutbound: 10, balance: 10 },
+      { productName: 'Product D', productCode: '3fa85f64-5717-4562-b3fc-2c963f66afa6', movementDate: new Date(2025, 3, 12), totalInbound: 10, totalOutbound: 5, balance: 5 },
+      { productName: 'Product E', productCode: '3fa85f64-5717-4562-b3fc-2c963f66afa6', movementDate: new Date(2025, 3, 12), totalInbound: 15, totalOutbound: 5, balance: 10 },
+      { productName: 'Product F', productCode: '3fa85f64-5717-4562-b3fc-2c963f66afa6', movementDate: new Date(2025, 3, 12), totalInbound: 25, totalOutbound: 10, balance: 15 },
+      { productName: 'Product G', productCode: '3fa85f64-5717-4562-b3fc-2c963f66afa6', movementDate: new Date(2025, 3, 12), totalInbound: 30, totalOutbound: 20, balance: 10 },
+      { productName: 'Product H', productCode: '3fa85f64-5717-4562-b3fc-2c963f66afa6', movementDate: new Date(2025, 3, 12), totalInbound: 40, totalOutbound: 30, balance: 10 },
     ];
   }
 
