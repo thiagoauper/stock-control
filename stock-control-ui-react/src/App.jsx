3d50e592f5f24588a364639a65099a2324d42dfe
Thiago Perine <thiagoauper@gmail.com>
2025-05-31 13:42:04 -0300

    Allowing user to select theme.

--- stock-control-ui-react/src/App.jsx
+++ stock-control-ui-react/src/App.jsx
@@ -6,14 +6,24 @@ import StockReport from './modules/features/reports/components/stock-report/stoc
 function App() {
   const [displayProductMovements, setDisplayProductMovements] = useState(true);
 
-  const content = 
+  const content =
     displayProductMovements
       ? <AddProductMovement />
       : <StockReport />;
 
   return (
     <>
-      <h1 id="mainTitle">Welcome to the Stock Control System</h1>
+      <div style={{ display: 'flex', margin: '20px' }}>
+        <div>
+          <h1 id="mainTitle">Welcome to the Stock Control System</h1>
+        </div>
+        <div style={{ flexAlign: 'right', marginLeft: 'auto', marginRight: '20px' }}>
+          <select style={{ width: '150px' }} id="themeSelector" class="form-control" onClick={() => { setTheme(document.querySelector('#themeSelector').value) }}>
+            <option value="dark">Dark mode</option>
+            <option value="light">Light mode</option>
+          </select>
+        </div>
+      </div>
 
       <div className='home-page-buttons-container'>
         <div className={`home-page-button ${displayProductMovements ? 'selected' : ''}`} onClick={() => setDisplayProductMovements(true)}>
