266761374f5d77e82b0117270bf022db251b8c7d
Thiago Perine <thiagoauper@gmail.com>
2025-06-02 16:43:28 -0300

    Displaying Stock Report data. TBC.

--- stock-control-ui-react/src/modules/features/reports/components/stock-report/stock-report.jsx
+++ stock-control-ui-react/src/modules/features/reports/components/stock-report/stock-report.jsx
@@ -1,19 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function StockReport() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [stockReportData, setStockReportData] = useState(null);
+
     useEffect(() => {
 
         //TODO: Move this to a service file
         async function getStockReport() {
             try {
+                setIsLoading(true);
                 const response = await fetch('http://localhost:5053/api/StockReport/2025-06-02');
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
                 const data = await response.json();
                 console.log(data); // Process the stock report data as needed
+                setStockReportData(data);
+                setIsLoading(false);
             } catch (error) {
                 console.error('Error fetching stock report:', error);
+                setIsLoading(false);
             }
         }
         getStockReport();
@@ -23,6 +30,15 @@ export default function StockReport() {
         <div>
             <h1>Stock Report</h1>
             {/* Add your components for displaying stock report here */}
+
+            {isLoading && <p>Loading...</p>}
+            {!isLoading && stockReportData &&
+                stockReportData.map(product => (
+                    <div key={product.productCode}>
+                        <h2>{product.productName}</h2>
+                        <p>Balance: {product.balance}</p>
+                    </div>
+                ))}
         </div>
     );
 }
