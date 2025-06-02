937cfd4c22245e66e1dab531e7429bf88be89e49
Thiago Perine <thiagoauper@gmail.com>
2025-06-02 16:17:09 -0300

    Fetching Stock Report. TBC (To Be Continued).

--- stock-control-ui-react/src/modules/features/reports/components/stock-report/stock-report.jsx
+++ stock-control-ui-react/src/modules/features/reports/components/stock-report/stock-report.jsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+
 export default function StockReport() {
+    useEffect(() => {
+
+        //TODO: Move this to a service file
+        async function getStockReport() {
+            try {
+                const response = await fetch('http://localhost:5053/api/StockReport/2025-06-02');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                console.log(data); // Process the stock report data as needed
+            } catch (error) {
+                console.error('Error fetching stock report:', error);
+            }
+        }
+        getStockReport();
+    }, []);
+
     return (
         <div>
             <h1>Stock Report</h1>
